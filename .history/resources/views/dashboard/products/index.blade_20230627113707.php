96b1fb89f80f088f334529623ffccef7ec43954b
aliakbarmotallebi <motallebi.aliakbar@gmail.com>
2023-06-27 12:10:28 +0330

    install select2

---
+++ .history/resources/views/dashboard/products/index.blade_20230627113707.php
@@ -0,0 +1,79 @@
+@extends('dashboard.layouts.base')
+@section('title', 'مدیریت محصولات')
+@section('content')
+
+<x-dashboard.main title="مدیریت محصولات">
+  <x-slot name="header"></x-slot>
+  <x-slot name="append"></x-slot>
+  <div class="flex items-center justify-between w-full border border-gray-100 px-10 bg-white rounded-xl mb-5">
+    <form class="grid grid-cols-8 w-full py-5" method="GET">
+      <div class="grid col-span-8 md:col-span-4 lg:col-span-3 xl:col-span-2">
+        <div class="mr-4">
+            <label class="block mb-2">
+              گروه فرعی
+            </label>
+            <select name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+              <option value="">
+                  دسته بندی را انتخاب کنید
+              </option>
+              @foreach($subcategories as $category)
+                <option value="{{ $category->erp_code }}" {{ (request()->get('category') == $category->erp_code) ? "selected" : ""}}>
+                  {{ $category->name }}
+                </option>
+              @endforeach
+            </select>
+        </div>
+      </div>
+      <div class="grid col-span-8 md:col-span-4 lg:col-span-3 xl:col-span-2">
+        <div class="mr-4 block mb-2">
+            <label class="block mb-2">جستجو</label>
+            <input name="search" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="نام ، بارکد" value="{{ request()->get('search') }}">
+        </div>
+      </div>
+      <div class="grid col-span-8 md:col-span-4 lg:col-span-3 xl:col-span-2">
+        <div class="mr-4">
+          <label class="block mb-2">فیلتر</label>
+
+          <select name="filter" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <option value="">
+                 فیلتر را انتخاب کنید
+            </option>
+            <option value="1" {{ (request()->get('filter') == 1) ? "selected" : ""}}>
+              محصولات ویژه
+            </option>
+            <option value="2" {{ (request()->get('filter') == 2) ? "selected" : ""}}>
+              بدون عکس
+            </option>
+                          <option value="3" {{ (request()->get('filter') == 3) ? "selected" : ""}}>
+              بدون عکس
+              و موجود
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="grid col-span-8 md:col-span-4 lg:col-span-3 xl:col-span-2 pt-4 md:pt-0">
+          <div class="mr-auto self-end">
+              <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none">
+                جستجو کن
+              </button>
+              <a href="{{ route('dashboard.products.index') }}" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">
+                  پاک
+              </a>
+          </div>
+      </div>
+  </form>
+  </div>
+  <div class="grid grid-cols-12 gap-2">
+      @foreach($products as $product)
+        @livewire('product-card', [
+          'product' => $product
+        ])
+      @endforeach
+  </div>
+  <div class="pt-20">
+    {{ $products->appends(request()->query())->links('vendor.pagination.tailwind') }}
+  </div>
+</x-dashboard.main>
+
+
+@endsection
---
+++ .history/resources/views/dashboard/settings/units/index.blade_20230515150506.php
@@ -0,0 +1,67 @@
+@extends('dashboard.layouts.base')
+@section('title')
+            لیست واحد های سیستم
+@endsection
+
+@section('content')
+<x-dashboard.main title="لیست واحد های سیستم">
+    <x-slot name="header"></x-slot>
+    <x-slot name="append">
+        @include('dashboard.settings._tabs')
+    </x-slot>
+    <x-dashboard.card title="لیست واحد های سیستم">
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        ERO_CODE
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        نام واحد
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        مقدار اصلی
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        مفدار محاسبه
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        حداقل مقدار سفارش
+                    </th>
+                    <th scope="col" class="px-6 py-3"></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($units as $unit)
+                <tr class="bg-white border-b">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center">
+                        {{ $unit->erp_code }}
+                    </th>
+                    <td class="px-6 py-4 text-center">
+                        {{ $unit->name }}
+                    </td>
+                    <td class="px-6 py-4 text-center">
+                        {{ $unit->few }}
+                    </td>
+                    <td class="px-6 py-4 text-center">
+                        {{ $unit->arithmetic_few }}
+                    </td>
+                    <td class="px-6 py-4 text-center">
+                        {{ $unit->min_few }}
+                    </td>
+                    <td class="px-6 py-4">
+                        <div class="flex items-center justify-end">
+                            <a href="{{ route('dashboard.units.edit', $unit) }}" target="_blank" class="ml-1 flex items-center p-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 focus:outline-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M15.728 9.686l-1.414-1.414L5 17.586V19h1.414l9.314-9.314zm1.414-1.414l1.414-1.414-1.414-1.414-1.414 1.414 1.414 1.414zM7.242 21H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L7.243 21z"/></svg>
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </x-dashboard.card>
+</x-dashboard.main>
+@endsection
